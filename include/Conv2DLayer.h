aa4ff52c31301a6d9865617d113a0a6d664b9a31
jr19f <jan.richter@telematik-zentrum.de>
2020-05-10 09:11:36 +0200

    convolution layer

---
+++ include/Conv2DLayer.h
@@ -0,0 +1,14 @@
+//
+// Created by janphr on 10.05.20.
+//
+
+#ifndef FRAMEWORK_CONV2DLAYER_H
+#define FRAMEWORK_CONV2DLAYER_H
+
+
+class Conv2DLayer {
+
+};
+
+
+#endif //FRAMEWORK_CONV2DLAYER_H
---
+++ src/Conv2DLayer.cpp
@@ -0,0 +1,5 @@
+//
+// Created by janphr on 10.05.20.
+//
+
+#include "Conv2DLayer.h"
